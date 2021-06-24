c22f39875ff8dd63e1ad195e19aea9e30815628b
Bruno Gonzalez <bruno_nicolas88@hotmail.com>
2021-06-24 19:24:21 -0300

    Termine el js de detalle de album

--- javascript/detail-album.js
+++ javascript/detail-album.js
@@ -23,34 +23,38 @@ window.addEventListener("load", function(){
             })
             .then(function (datos){
                 let album_1 = document.querySelector('#album_1');
-                album_1.innerHTML += `<img src="${datos.cover_big}"><h2>${datos.artist.name}</h2><h2>${datos.title}</h2><h2>${datos.release_date}</h2><h2>${datos.genres.data[0].name}</h2><h2>${datos.tracks.data[0].title}</h2>`;
+                album_1.innerHTML += `
+                <div>
+                    <img src="${datos.cover_big}"><h2>Artista:  ${datos.artist.name}</h2><h2>Titulo del album:  ${datos.title}</h2>
+                    <h2>Año de publicacion:  ${datos.release_date}</h2><h2>Genero:  ${datos.genres.data[0].name}</h2>
+                </div>`;
             })
             .catch(function(error){
                 console.log("el error es:" + error);
             });
     }
 
-    // function mostrar_canciones_album(idAlbum){
+    function mostrar_canciones_album(idAlbum){
 
-    //     let api_url = `${proxy}https://api.deezer.com/album/${idAlbum}`;
+        let api_url = `${proxy}https://api.deezer.com/album/${idAlbum}`;
 
-    //     fetch(api_url)
-    //         .then(function (response){
-    //             return response.json();
-    //         })
-    //         .then(function (datos){
-    //             for(i=0; i<= 4; i++){
-    //                 document.querySelector('#lista_canciones').innerHTML += `
-    //                 <div class="canciones" id="cancion_${i}">
-    //                     <img src="${dato.cover_medium}">
-    //                     <h2>${datos.tracks[i].title}</h2>
-    //                 </div>`;
-    //             }
-    //         })
-    //         .catch(function(error){
-    //             console.log("el error es:" + error);
-    //         });
-    // }
+        fetch(api_url)
+            .then(function (response){
+                return response.json();
+            })
+            .then(function (datos){
+                for(i=0; i<= 10; i++){
+                    document.querySelector('#lista_canciones').innerHTML += `
+                    <div class="canciones" id="cancion_${i}">
+                        <img src="${datos.cover_medium}">
+                        <h2>${datos.tracks.data[i].title}</h2>
+                    </div>`;
+                }
+            })
+            .catch(function(error){
+                console.log("el error es:" + error);
+            });
+    }
 
 
 
